4efe058bafc2c0fb13509b107ac10bdc71866655
Christian Bianchi <whites11@gmail.com>
2022-03-09 11:52:50 +0100

    Initial commit

---
+++ integration/templates/azure_ccm_values.go
@@ -0,0 +1,9 @@
+//go:build k8srequired
+// +build k8srequired
+
+package templates
+
+// AzureCloudControllerManagerValues values required by azure-cloud-controller-manager-app.
+const AzureCloudControllerManagerValues = `
+name: azure-cloud-controller-manager-test
+`
